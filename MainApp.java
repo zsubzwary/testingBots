13a4f668541d06b993eee0fc169b1880869d2ab7
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-03-24 13:11:46 +0500

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -13,6 +13,15 @@ public class MainApp {
  */
     public static void main(String[] args){
         // @todo implement this method !! 
+        
+        System.out.println("Hi");
+        
+        
+        /**
+        * @todo Take over the world
+        * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+        */
+        
     }
 
 
